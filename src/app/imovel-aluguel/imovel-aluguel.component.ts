7d2f68f1b0b9e4f32baedfb77dea7a576bceaa0f
NatsuuxD <ddkgabriel1@gmail.com>
2024-07-18 13:48:37 -0300

    Add files via upload

--- src/app/imovel-aluguel/imovel-aluguel.component.ts
+++ src/app/imovel-aluguel/imovel-aluguel.component.ts
@@ -20,5 +20,9 @@ export class ImovelAluguelComponent {
     const id=this.route.snapshot.params["id"]
     this.imovel = this.imvService.buscaImovelPeloId(id)
   }
+  voltarDetalhes(){
+    const url = `/detalhes/${this.imovel?.id}`
+    this.rt.navigate([url])
+  }
   
 }
